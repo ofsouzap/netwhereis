45cd6b4504bdba8343e2bcdf6baa045fd2a93d76
ofsouzap <ofsouzap@gmail.com>
2023-02-20 23:14:16 +0000

    Add uninstall script

---
+++ uninstall.sh
@@ -0,0 +1,21 @@
+# Bash script to uninstall the netwhereis daemon from the system
+
+SCRIPT_FILENAME="netwhereisd.py"
+SERVICE_FILENAME="netwhereisd.service"
+SERVICE_PATH="/etc/systemd/system/$SERVICE_FILENAME"
+
+# Stop service
+
+sudo systemctl stop netwhereisd.service
+
+# Remove service file
+
+sudo rm "$SERVICE_PATH"
+
+# Remove script from /usr/bin
+
+sudo rm "/usr/bin/$SCRIPT_FILENAME"
+
+# Reload systemctl daemons
+
+sudo systemctl daemon-reload
